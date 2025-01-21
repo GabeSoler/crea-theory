a77ca6f8e6c20d9fe078e93c3d288c78da399551
Gabriel Soler <gsoler@gmail.com>
2025-01-21 11:58:34 +0000

    trying new coltrane error in quote marks in local script

--- site/gunicorn.conf.py
+++ site/gunicorn.conf.py
@@ -10,10 +10,3 @@ workers = multiprocessing.cpu_count() * 2 + 1
 
 workers = 2
 
-
-def when_ready(server):
-    # touch app-initialized when ready
-    open("/tmp/app-initialized", "w").close()
-
-bind = "unix:///tmp/nginx.socket"
-workers = 3
